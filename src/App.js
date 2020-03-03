da0572d76dd692607b82cf07d06d003f5d71a34d
root <root@DESKTOP-9AMNB8N.localdomain>
2020-03-03 00:23:29 -0400

    Initial commit of displaying high res images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import OpenSeaDragon from 'openseadragon';
 import './App.css';
 
+let externalPath = "../public/images/602.dzi";
+let internalPath = "./images/602.dzi";
+
 function App() {
+  OpenSeaDragon({
+        id: "openseadragon1",
+        prefixUrl: "/img/openseadragon/",
+        tileSources: internalPath,
+      });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
